a2595fd02f091b98ab15ef864c6064950513ffa9
Pooja <kp.pwc16@gmail.com>
2020-08-02 00:01:38 +0530

    more functionality added

---
+++ download.php
@@ -0,0 +1,19 @@
+ <?php
+
+       $f="MyResume.pdf";   
+
+       $file = ("assets/$f");
+
+       $filetype=filetype($file);
+
+       $filename=basename($file);
+
+       header ("Content-Type: ".$filetype);
+
+       header ("Content-Length: ".filesize($file));
+
+       header ("Content-Disposition: attachment; filename=".$filename);
+
+       readfile($file);
+
+  ?>    
